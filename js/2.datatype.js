a8e5c6c36ff896064c58d3fc78092696f80d2a56
Sukhmanjeet Singh <149586206+Sukhmanjeet1998@users.noreply.github.com>
2024-03-08 12:54:20 +0000

    datatypes and ecma script

--- js/2.datatype.js
+++ js/2.datatype.js
@@ -3,3 +3,20 @@
 "use strict";  // treat all js code as newer version
 // alert("Hello");
 
+// Primitive Datatypes.....
+// nn bb ss u
+// null
+// number
+// boolean
+// bigint
+// symbol
+// string
+// undefined
+
+
+// Non Primitive Datatypes.....
+// object
+
+
+console.log(typeof undefined);
+console.log(typeof null);

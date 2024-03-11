201bd191b401c1b0af5677e499466c4098ceefcd
Sukhmanjeet Singh <149586206+Sukhmanjeet1998@users.noreply.github.com>
2024-03-11 11:15:12 +0000

    operator & comparison opr

---
+++ js/4.operator.js
@@ -0,0 +1,58 @@
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2/3);
+// console.log(2**3);
+// console.log(2%3);
+
+// console.log("HELLO" + " SIR");
+// console.log("1" + 2+2);
+// console.log(2 + 2+"1");
+
+// Comparison operator...............
+
+// console.log(2>4);
+// console.log(2<4);
+// console.log(2>=4);
+// console.log(2<=4);
+// console.log(2!=4);
+// console.log(2==4);
+
+// console.log("2">1);
+
+/*
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+console.log(null < 0);
+console.log(null <= 0);
+*/
+
+/*
+Null: In JavaScript, null is a special value that represents the intentional absence of any object value. It is a primitive value that indicates a variable has been defined but has no value at all. When a variable is assigned null, it means it explicitly does not point to any object, array, string, etc.
+
+let x = null;
+console.log(x); // Output: null
+Undefined: In JavaScript, undefined is a primitive value automatically assigned to variables that have been declared but not yet assigned a value. It also represents the value returned by functions that do not explicitly return anything. It signifies the absence of a value in a variable, typically due to the variable not being initialized.
+
+let y;
+console.log(y); // Output: undefined
+Empty: "Empty" is a more general term and can refer to different things in different contexts.
+
+Empty String: An empty string ('') is a string that contains zero characters. It's a valid string with a length of 0.
+
+let emptyString = '';
+console.log(emptyString); // Output: ''
+Empty Array: An empty array ([]) is an array that contains no elements.
+
+let emptyArray = [];
+console.log(emptyArray); // Output: []
+Empty Object: An empty object ({}) is an object that contains no properties.
+
+let emptyObject = {};
+console.log(emptyObject); // Output: {}
+Empty Value: This could refer to a variable being uninitialized or having a null or undefined value. It means the variable exists, but it does not contain any meaningful value.
+
+In summary, null represents the absence of an object value, undefined represents the absence of any value, and "empty" can refer to various specific contexts such as an empty string, array, or object.
+*/
+

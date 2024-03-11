74ebfca34982b64ce21fc7e11a4dfb09fc93d09a
Sukhmanjeet Singh <149586206+Sukhmanjeet1998@users.noreply.github.com>
2024-03-11 10:27:31 +0000

    Type COnversion

---
+++ js/3.typeconversion.js
@@ -0,0 +1,30 @@
+let score = "100";
+// console.log(score, typeof(score));
+score = Number(score)
+// console.log((score), typeof(score));
+
+let invalid_num = "1001_";
+invalid_num = Number(invalid_num)
+// console.log((invalid_num), typeof(invalid_num));
+
+let num = null;
+// console.log(num, typeof(num));
+
+let num_ = "";
+// console.log(num_, typeof(num_));
+
+let _num = {};
+// console.log(_num, typeof(_num));
+
+let _arr = [];
+// console.log(_arr, typeof(_arr));
+
+let isvalue =  Boolean(1);
+let is_value = Boolean(0);
+let is_empty = Boolean("");
+let not_empty = Boolean("Hello");
+
+// console.log(isvalue, typeof(isvalue));
+// console.log(is_value, typeof(is_value));
+// console.log(is_empty, typeof(is_empty));
+// console.log(not_empty, typeof(not_empty));

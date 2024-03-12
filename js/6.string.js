94daf0c449b53f6ba2eb8d312f35a426b67803cb
Sukhmanjeet Singh <149586206+Sukhmanjeet1998@users.noreply.github.com>
2024-03-12 20:37:04 +0000

    string

---
+++ js/6.string.js
@@ -0,0 +1,32 @@
+// console.log("String");
+
+let _myname = "Sukhmanjeet";
+let sirname = "Singh";
+
+// console.log(_myname+sirname);
+
+let myname = _myname+sirname;
+// console.log(myname[0]);
+
+// String Literals
+// String interpolation is injecting a expresion in a string
+
+// console.log(`Hi my name is ${myname}`);
+
+let city = new String("shahjahanpur");
+// console.log(city);
+
+// console.log(city, city.length);
+// console.log(city.toLocaleUpperCase());
+
+// console.log(city.charAt(4));
+// console.log(city.indexOf('f'));
+// console.log(city.indexOf('j'));
+
+let new_slice_str = city.slice(0,5);
+// console.log(new_slice_str);
+
+
+
+
+

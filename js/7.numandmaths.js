36d5376e181bf22d7eb6b19b450e9f044bb73cfc
Sukhmanjeet Singh <149586206+Sukhmanjeet1998@users.noreply.github.com>
2024-03-13 11:14:05 +0000

    math and num

---
+++ js/7.numandmaths.js
@@ -0,0 +1,38 @@
+// Number...................................
+
+let score = 200;
+// console.log(score);
+// console.log(score.toString().length);
+
+let salary = 500;
+// console.log(salary);
+// console.log(salary.toFixed(2));
+
+let precise_num = 133.68;
+// console.log(precise_num.toPrecision(2));
+// console.log(precise_num.toPrecision(3));
+// console.log(precise_num.toPrecision(4));
+// console.log(precise_num.toPrecision(5));
+
+
+// Math....................................
+
+// console.log(Math);
+// console.log(Math.abs(-10));
+// console.log(Math.round(5.5));
+// console.log(Math.ceil(5.1));
+// console.log(Math.floor(5.9));
+// console.log(Math.min(4,2,9,1,8));
+// console.log(Math.max(4,2,9,1,8));
+
+/*
+console.log(Math.random());  // give value between 0 & 1
+console.log(Math.random()*10);  // can give 0 also
+console.log((Math.random()*10)+1);  // can't give 0 start with 1
+console.log(Math.floor((Math.random()*10)+1));
+
+let max = 20;
+let min = 10;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+*/

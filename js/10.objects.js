c8c0f83a9484cdc0c81c3c0af8ea0b14325b8cbf
Sukhmanjeet Singh <149586206+Sukhmanjeet1998@users.noreply.github.com>
2024-03-14 09:05:53 +0000

    objects

---
+++ js/10.objects.js
@@ -0,0 +1,17 @@
+// Objects
+
+// Singeton (in future we learn) (object.create)
+
+// Obect literals
+const Jsuser = {
+    name_ : "Sukh",
+    age : 25,
+    location : "Jaipur",
+    email : "sukh@def.com",
+    isloggedIn : false,
+    lastLoginDays : ["Monday","Saturday"]
+};
+
+// access the objects property 
+console.log(Jsuser.email);
+console.log(Jsuser[email]);
